2f039d9d1a5c7ecb113030d236f9412131a22484
tiggy31 <45244793+tiggy31@users.noreply.github.com>
2022-01-19 21:23:37 -0500

    Created file

---
+++ Flavor-recipe 2/Flavor-recipe 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flavor-recipe 2
+//
+//  Created by tigist_alemu on 1/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Flavor-recipe 2/Flavor-recipe 2UITests/Flavor_recipe_2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Flavor_recipe_2UITestsLaunchTests.swift
+//  Flavor-recipe 2UITests
+//
+//  Created by tigist_alemu on 1/19/22.
+//
+
+import XCTest
+
+class Flavor_recipe_2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
